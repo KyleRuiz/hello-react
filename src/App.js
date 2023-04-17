1928c4378b2dd2eff4542ed456d2717c3d5216c6
Kyle <kyleruiz1515@gmail.com>
2023-04-17 22:30:51 +0900

    6.2 데이터 배열을 컴포넌트 배열로 변환하기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { Component } from "react";
-import ScrollBox from "./ScrollBox";
+import React from "react";
+import MyComponent from "./MyComponent";
+import Counter from "./Counter";
+import Say from "./Say";
+import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
+import IterationSample from "./IterationSample";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <MyComponent favoriteNumber={26}> 리액트</MyComponent> <br />
+      <Counter /> <br />
+      <Say /> <br />
+      <EventPractice /> <br />
+      <ValidationSample /> <br />
+      <IterationSample /> <br />
+    </>
+  );
+};
 
 export default App;
