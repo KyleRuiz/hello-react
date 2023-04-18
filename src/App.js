6e8817d539e56e0570b537999b4ca7f3e72c1d63
Kyle <kyleruiz1515@gmail.com>
2023-04-19 00:26:21 +0900

    7.3 라이프사이클 메서드 사용하기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from "react";
-import MyComponent from "./MyComponent";
-import Counter from "./Counter";
-import Say from "./Say";
-import EventPractice from "./EventPractice";
-import ValidationSample from "./ValidationSample";
-import IterationSample from "./IterationSample";
+import React, { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
 
-const App = () => {
-  return (
-    <>
-      <MyComponent favoriteNumber={26}> 리액트</MyComponent> <br />
-      <Counter /> <br />
-      <Say /> <br />
-      <EventPractice /> <br />
-      <ValidationSample /> <br />
-      <IterationSample /> <br />
-    </>
-  );
-};
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
+}
 
 export default App;
